4fb25826a8c5f4c22afad04c6bb866cd45ac56b0
Julie Descloitres <julie.descloitres@gmail.com>
2024-07-11 17:09:08 +0200

    first version of main

--- hydrate_algorithm_no_interface.py
+++ hydrate_algorithm_no_interface.py
@@ -3,6 +3,8 @@
 # import math, scipy
 from scipy.integrate import quad
 from math import exp, pi, log, sqrt, cos
+import numpy as np
+from scipy.special import cbrt
 
 ### Definition of constants
 
@@ -11,13 +13,15 @@ R_IDEAL_GAS = 8.314
 T_ZERO = 273.15
 P_ZERO = 0
 
+MAX_DIFFERENCE = 10E-5
+
 # Inside structure dictionary
 KEY_FOR_MU0 = 'deltaMu0'
 KEY_FOR_H0 = 'deltaH0'
 KEY_FOR_CP0 = 'deltaCp0'
 KEY_FOR_B = 'b'
 KEY_FOR_V0 = 'deltaV0'
-KEY_FOR_CAVITIES = 'cavities'
+KEY_FOR_CAVITIES = 'cavities'       # list (2 items) of lists containing cavities caracteristics
 
 # Inside 'cavities' value (list of lists) of structure dictionnary
 INDEX_FOR_NU = 0            # index pointing to nu (number of this type of cavity) in the cavity info table in the structure info table
@@ -72,25 +76,26 @@ def deltah_L(Temp, structure: dict):
 
 # non ideality term ln(a_w)
 
-def a_w(T, P, components: list):
+def a_w(T, P, components: list, coefficients: list):
     sumj = 0
     for j in range(len(components)):
         Vinf = components[j][KEY_FOR_VINF]
         K1 = components[j][KEY_FOR_K1]
         K2 = components[j][KEY_FOR_K2]
+        print(K1, K2)
         H = exp(K1 + K2/T)
-        fj = fugacity_j(T, P, j)
+        fj = fugacity_j(T, P, components, coefficients, j)
         sumj += fj / (H * exp( P*Vinf / (R_IDEAL_GAS*T)))
     return 1 - sumj
 
 
 # Injecting in deltaMu_L
 
-def deltaMu_L(T, P , structure: dict, components: list):
+def deltaMu_L(T, P , structure: dict, components: list, coefficients: list):
     deltaMu0 = structure[KEY_FOR_MU0]
     deltah = deltah_L(T, structure)
     deltaV0 = structure[KEY_FOR_V0]
-    aw = a_w(T, P, components)
+    aw = a_w(T, P, components, coefficients)
 
     termMu = T * deltaMu0 / T_ZERO
 
@@ -122,20 +127,24 @@ def langmuir_ij(T, structure_cavities: list, components: list, i: int, j: int) -
     def integrand(r):
 
         # definition of w(r)
-        def w(r):
+        def w_r(r):
             # definition of delta_N
             def delta_N(N):
                 return ( (1 - r/R - a/R)**(-N) - (1 + r/R - a/R)**(-N) ) / N
 
             return ( 2 * z * epsilon
                     * ( (sigma**12 / (r * R**11)) * (delta_N(10) + delta_N(11) * a/R )
-                        (sigma** 6 / (r * R** 5)) * (delta_N( 4) + delta_N( 5) * a/R ) )
+                    -   (sigma** 6 / (r * R** 5)) * (delta_N(4) + delta_N(5) * a/R ) )
             )
 
-        return exp( - w(r) / (K_BOLTZMANN * T)) * r**2
+        # print('w : ', w_r(r))
+        return np.exp( - w_r(r) / (K_BOLTZMANN * T)) * r**2
     integral = quad(integrand, 0, R)[0]
+    # print('0.1 :', integrand(0.1), 'R :', integrand(1), R)
 
-    return 4 * pi * integral  / (K_BOLTZMANN * T)
+    # print(integral)
+    return 4 * pi * 1  / (K_BOLTZMANN * T)
+    # return 4 * pi * integral  / (K_BOLTZMANN * T)
 
 # a_m
 
@@ -202,37 +211,45 @@ def lnphi_j(T, Pres, components: list, coefficients: list, j: int):
     m = q - (p**2) / 3
     n = r + (2 * p**3 - 9*p*q) / 27
     delt = (n**2)/4 + (m**3) / 27
+
     if delt > 0:
         Zj = (- p / 3
-             + (sqrt(delt) - n/2) ** (1/3)
-             + (- sqrt(delt) - n/2) ** (1/3)
+             + cbrt(sqrt(delt) - n/2)
+             + cbrt(- sqrt(delt) - n/2)
         )
+
     elif delt < 0:
         angle = - (n / abs(n)) * sqrt( (- 27 * n**2) / (4 * m**3) )
         Zj = ( - p / 3
             + 2 * sqrt(-m / 3) * cos(angle/3)
         )
-
     else:
         print('Error: no known value of Z for delta = 0')
         # TODO raise error here
 
+    # print(Zj, B, B/Zj)
+    # print(( (bj / bm) * (Zj - 1)
+    #         - log(abs(Zj - B))
+    #         - (A / B) * (2 * sqrt(aj/am) - bj/bm ) * log(abs(1 + B/Zj))
+    # ))
     return ( (bj / bm) * (Zj - 1)
-            - log(Zj - B)
-            - (A / B) * (2 * sqrt(aj/am) - bj/bm ) * log(1 + B/Zj)
+            - log(abs(Zj - B))
+            - (A / B) * (2 * sqrt(aj/am) - bj/bm ) * log(abs(1 + B/Zj))
     )
+
 
 # fj
 
 def fugacity_j(T, P, components: list, coefficients, j: int):
-    phij = exp(lnphi_j(T, P, components, coefficients, j))
+    # phij = exp(lnphi_j(T, P, components, coefficients, j))
+    phij = exp(-lnphi_j(T, P, components, coefficients, j))
     yj = components[j][KEY_FOR_Y]
     return phij * yj * P
 
 
 # Injecting in deltaMu_H
 
-def deltaMu_H(T, P, structure_cavities: list, components: list):
+def deltaMu_H(T, P, structure_cavities: list, components: list, coefficients: list):
     sumi = 0
     for i in range(len(structure_cavities)):
         sumj = 0
@@ -240,7 +257,7 @@ def deltaMu_H(T, P, structure_cavities: list, components: list):
             cij = langmuir_ij(T, structure_cavities, components, i, j)
 
             # TODO ARGUMENTS TO BE MODIFIED ONCE DEF IS UPDATED
-            fj = fugacity_j(T, P, j)
+            fj = fugacity_j(T, P, components, coefficients, j)
 
             sumj += cij * fj
         nui = structure_cavities[i][INDEX_FOR_NU]
@@ -251,17 +268,17 @@ def deltaMu_H(T, P, structure_cavities: list, components: list):
 
 # thetaij
 
-def theta_ij(T, P, structure_cavities: list, components: list, i: int, j:int) -> float:
+def theta_ij(T, P, structure_cavities: list, components: list, coefficients: list, i: int, j:int) -> float:
     sum = 0
     for k in range(len(components)):
         cik = langmuir_ij(T, structure_cavities, components, i, k)
 
         # TODO ARGUMENTS TO BE MODIFIED ONCE DEF IS UPDATED
-        fk = fugacity_j(T, P, k)
+        fk = fugacity_j(T, P, components, coefficients, k)
 
         sum += cik * fk
     cij = langmuir_ij(T, structure_cavities, components, i, j)
-    fj = fugacity_j(T, P, j)
+    fj = fugacity_j(T, P, components, coefficients, j)
 
     return cij * fj / (1 + sum)
 
@@ -269,16 +286,102 @@ def theta_ij(T, P, structure_cavities: list, components: list, i: int, j:int) ->
 ## Optimisation (Kihara parameters)
 
 
+
+
 ## Determination of hydrate composition xj_H
 
+def xj_H(T, P, structure_cavities: list, components: list, coefficients: list, j:int):
+
+    sumk = 0
+    for k in range(len(components)):
+        sumi = 0
+        for i in range(len(structure_cavities)):
+            nui = structure_cavities[i][INDEX_FOR_NU]
+            thetaik = theta_ij(T, P, structure_cavities, components, coefficients, i, k)
+            sumi += nui * thetaik
+        sumk += sumi
+
+    sumi2 = 0
+    for i in range(len(structure_cavities)):
+        nui = structure_cavities[i][INDEX_FOR_NU]
+        thetaij = theta_ij(T, P, structure_cavities, components, coefficients, i, j)
+        sumi2 += nui * thetaij
+
+    return sumi2 / sumk
+
+def xj_H(structure_cavities: list, components: list, thetas: list, j:int):
+
+    sumk = 0
+    for k in range(len(components)):
+        sumi = 0
+        for i in range(len(structure_cavities)):
+            nui = structure_cavities[i][INDEX_FOR_NU]
+            thetaik = thetas[i][k]
+            sumi += nui * thetaik
+        sumk += sumi
+
+    sumi2 = 0
+    for i in range(len(structure_cavities)):
+        nui = structure_cavities[i][INDEX_FOR_NU]
+        thetaij = thetas[i][j]
+        sumi2 += nui * thetaij
+
+    return sumi2 / sumk
 
 
+#### Algorithm
+
+# TODO put this main in the file with the interface
+def main(T, P, structure, components, coefficients, results: dict):
+    deltaMuH = deltaMu_H(T, P, structure[KEY_FOR_CAVITIES], components, coefficients)
+    deltaMUL = deltaMu_L(T, P, structure, components, coefficients)
+    results['Temperature'] = T
+    if abs(deltaMuH - deltaMUL) <= MAX_DIFFERENCE:
+        results['Pressure'] = determineFinalValues(T, P, structure, components, coefficients)[0]
+        results['Thetas'] = determineFinalValues(T, P, structure, components, coefficients)[1]
+        print(determineFinalValues(T, P, structure, components, coefficients))
+        # calculate x with all the values of theta
+    else:
+        print('no')
+
+def determineFinalValues(T, P, structure, components, coefficients):
+    Peq = P
+    theta = []
+    for j in range(len(components)):
+        thetaS = theta_ij(T=T, P=P, structure_cavities=structure[KEY_FOR_CAVITIES], components=components, coefficients=coefficients, i=0, j=j)
+        thetaL = theta_ij(T=T, P=P, structure_cavities=structure[KEY_FOR_CAVITIES], components=components, coefficients=coefficients, i=1, j=j)
+        # xjH = xj_H(T, P, structure[KEY_FOR_CAVITIES], components, j)
+        theta.append((thetaS, thetaL))
+    return Peq, theta
 
 ### TESTS
 
 # tab0 = [1, 2, 3]
 # tab1 = 4
-# tab = [tab0,tab1]
+# tab = tab0 + [tab1]
 
 
 # print(tab)
+
+T_test = 10
+P_test = 0.1
+y0_test = 0.4
+y1_test = 0.6
+results_test = {'Components' : [], 'Composition' : [], 'Temperature' : -1, 'Pressure' : -1, 'Structure' : '', 'Thetas' : []}
+cavities_test = [[None] * 3] * 2
+cavities_test[0][INDEX_FOR_NU] = 2
+cavities_test[1][INDEX_FOR_NU] = 8
+cavities_test[0][INDEX_FOR_R] = 391
+cavities_test[1][INDEX_FOR_R] = 433
+cavities_test[0][INDEX_FOR_Z] = 20
+cavities_test[1][INDEX_FOR_Z] = 24
+structure_test = {KEY_FOR_MU0 : 1299, KEY_FOR_H0 : 1861, KEY_FOR_CP0 : -38.12, KEY_FOR_B : 0.141, KEY_FOR_V0 : 3, KEY_FOR_CAVITIES : cavities_test}
+components_test = [{KEY_FOR_ID : 0, KEY_FOR_EPSILON : 154.54 * K_BOLTZMANN, KEY_FOR_SIGMA : 3.165, KEY_FOR_A : 0.3834, KEY_FOR_VINF : 32, KEY_FOR_K1 : 15.826277, KEY_FOR_K2 : -1559.0631, KEY_FOR_Y : y0_test, KEY_FOR_OMEGA : 0.0115, KEY_FOR_TC : 190.56, KEY_FOR_PC : 4.599},
+                   {KEY_FOR_ID : 1, KEY_FOR_EPSILON : 168.77 * K_BOLTZMANN, KEY_FOR_SIGMA : 2.9818, KEY_FOR_A : 0.6805, KEY_FOR_VINF : 32, KEY_FOR_K1 : 14.283146, KEY_FOR_K2 : -2050.3269, KEY_FOR_Y : y1_test, KEY_FOR_OMEGA : 0.2276, KEY_FOR_TC : 304.19, KEY_FOR_PC : 7.342}
+            ]
+coefficients_test = [[0,0.1107], [0.1107, 0]]
+
+main(T=T_test, P=P_test, structure=structure_test, components=components_test, coefficients=coefficients_test, results=results_test)
+print(results_test)
+
+# langmuir_ij(T_test, structure_test[KEY_FOR_CAVITIES], components_test, 0, 0)
